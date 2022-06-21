83bfb0926ddce8cac849aec0ebd0511d77125d0e
emannotava <anthony.langendoen@gmail.com>
2022-06-21 13:54:26 +0200

    Updated paths to QuickBMS Script

--- Unpack to MP3.bat
+++ Unpack to MP3.bat
@@ -1,4 +1,4 @@
-"Tools\quickbms.exe" "Tools\wavescan.bms" "Game Files" "Tools\Decoding"
+"Tools\quickbms.exe" "Tools\wav_scanner.bms" "Game Files" "Tools\Decoding"
 FOR %%b IN ("Game Files\*.BNK") DO ("Tools\bnkextr.exe" "%%b" & MOVE *.wav "Tools\Decoding")
 FOR %%c IN (Tools\Decoding\*.WAV) DO ("Tools\ww2ogg.exe" "%%c" --pcb Tools\packed_codebooks_aoTuV_603.bin & DEL "%%c")
 FOR %%d IN (Tools\Decoding\*.OGG) DO ("Tools\revorb.exe" "%%d" & MOVE "%%d" "MP3")
